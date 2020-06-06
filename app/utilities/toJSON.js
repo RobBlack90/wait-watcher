07089030218b473ccd46e6bcee3b22c0a951110e
blackr <RobBlack90@gmail.com>
2020-06-06 00:52:27 -0500

    deal with nested _ids

--- app/utilities/toJSON.js
+++ app/utilities/toJSON.js
@@ -1,15 +1,26 @@
 const klona = require('klona')
-const _ = require('lodash')
+
+function transform(obj) {
+  if (obj instanceof Array) {
+      for (let i = 0; i < obj.length; i++) {
+        obj[i] = transform(obj[i])
+      }
+  } else {
+      for (let prop in obj) {
+          if (prop === '_id') {
+            obj.id = obj._id
+            delete obj._id
+          } else if (prop === '__v') {
+            delete obj.__v
+          } else if (obj[prop] instanceof Array) {
+            obj[prop] = transform(obj[prop])
+          }
+      }
+  }
+  return obj
+}
 
 module.exports = function() {
-  // run toObject function on the model and make a copy of it
   let obj = klona(this.toObject())
-
-  // make _id  'id', but only if there isn't already an idea
-  if (!_.has(obj, 'id')) {
-    obj.id = obj._id
-  }
-
-  // return object without _id, __v
-  return _.omit(obj, ['_id', '__v'])
+  return transform(obj)
 }
