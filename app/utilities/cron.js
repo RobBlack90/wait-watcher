2abdecb6b2483c1063121181e0e87be44d295cbc
blackr <RobBlack90@gmail.com>
2020-06-27 12:40:50 -0500

    Up it to every 5mins. YOLO, ya know?

--- app/utilities/cron.js
+++ app/utilities/cron.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron')
 const changeDetector = require('./changeDetector')
 
-cron.schedule(`*/10 * * * *`, () => {
-  console.log(`Running Cron`)
+cron.schedule(`*/5 * * * *`, () => {
+  console.log(`Running Cron...`)
   changeDetector.detectChanges()
 })
