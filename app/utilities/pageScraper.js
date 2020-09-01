af602908f56c15d95e9b6ef4046a777f17298964
blackr <RobBlack90@gmail.com>
2020-09-01 16:10:59 -0500

    update headers

--- app/utilities/pageScraper.js
+++ app/utilities/pageScraper.js
@@ -3,7 +3,11 @@ const cheerio = require('cheerio')
 
 
 async function scrape(page) {
-    const response = await axios.get(page.url)
+    const response = await axios.get(page.url, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+        }
+    })
 
     if (response.status === 200) {
         const html = response.data
